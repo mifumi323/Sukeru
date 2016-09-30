dca9711ace9f293aeedccbdf3a0e2707122b51da
mifumi323 <mifumi323@tgws.fromc.jp>
2016-09-30 13:57:53 +0900

    とりあえず作ってみた

---
+++ Sukeru/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Media.Imaging;
+
+namespace MifuminSoft.Sukeru
+{
+    /// <summary>
+    /// MainWindow.xaml の相互作用ロジック
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            MouseLeftButtonDown += (sender, e) => { DragMove(); };
+        }
+
+        private void image_DragEnter(object sender, DragEventArgs e)
+        {
+            e.Effects = DragDropEffects.All;
+        }
+
+        private void image_Drop(object sender, DragEventArgs e)
+        {
+            var files = e.Data.GetData(DataFormats.FileDrop) as string[];
+            if (files != null && files.Length > 0)
+            {
+                try
+                {
+                    image.Source = new BitmapImage(new Uri(files[0]));
+                }
+                catch (Exception)
+                {
+                }
+            }
+        }
+    }
+}
